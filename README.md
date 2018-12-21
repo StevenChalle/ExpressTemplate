3c4135bdffd29af903b01907a35e683b7fe94bdf
WuShin <stchalle@gmail.com>
2018-12-21 16:26:21 +0100

    added express with single route, and nodemon

--- README.md
+++ README.md
@@ -5,5 +5,12 @@ API template with Express, using Sequelize with MySQL, and Joi for validation pa
 ## Installation
 
 On command line :
-- git clone https://github.com/StevenChalle/ExpressTemplate
-- npm i
+-> git clone https://github.com/StevenChalle/ExpressTemplate
+-> sudo npm install -g nodemon
+-> sudo npm install -g sequelize-cli
+-> sudo npm install -g mysql2
+-> npm i
+
+## Launching
+
+-> npm start
--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 
-//  get .env config
+// get .env config
 const dotenvConf = require('dotenv').config()
 if (dotenvConf.error) throw dotenvConf.error
+
+// create the server
+const express = require('express')
+const app = express()
+
+// add routes
+app.get('/', (req, res) => {
+  res.send('you just hit the home page\n')
+})
+
+// launch the server
+app.listen(process.env.SERVER_PORT || 3000, () => {
+  console.log(`-> Listening on port ${process.env.SERVER_PORT || 3000}...\n`)
+})
